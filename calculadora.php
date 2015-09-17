f40eb378c13eda1ec6dbd98eae51510c02765ade
Mother Fucker Elton <marco666@gmail.com>
2015-09-17 20:00:23 -0300

    atualizando calculadora

---
+++ calculadora.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+ <html lang = "pt-br">
+ 	<head>
+		<title>Calculadora </title>
+		<meta charset = "UTF 8">
+        </head>
+      <body>	
+	<form action="" method = "post">
+Primeiro numero: <input name "num1" type="text"><br>
+Segundo numero: <input name "num2" type="text"><br>
+<input type = "subimit" name = "operacao"value "+">
