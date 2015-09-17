acdc96233f0d05af8e9401a16b2175e98252d5bb
Mother Fucker Elton <marco666@gmail.com>
2015-09-17 20:44:45 -0300

    atualizando a calculadora

--- calculadora.php
+++ calculadora.php
@@ -9,3 +9,21 @@
 Primeiro numero: <input name "num1" type="text"><br>
 Segundo numero: <input name "num2" type="text"><br>
 <input type = "subimit" name = "operacao"value "+">
+	<?php
+	$a = $_POST ['num1'];
+        $b = $_POST ['num2'];
+        $op = $_POST [operacao];
+	
+	if(!empty($op)){
+         if($op == '+');
+ 		$c = &a + $b;
+		else if(
+			echo "Opcao invalida"
+			)
+		} 
+echo "Rsultado da soma: $c "
+}
+
+?>
+</body>
+</html>
